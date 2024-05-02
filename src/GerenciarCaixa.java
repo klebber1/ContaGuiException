d8cdcdbec14421cb0c3f3b0db7cf932cd350ea94
kleber <44203711+klebber1@users.noreply.github.com>
2024-05-02 10:38:41 -0300

    Update GerenciarCaixa.java

--- src/GerenciarCaixa.java
+++ src/GerenciarCaixa.java
@@ -6,13 +6,15 @@ import javax.swing.*;
 public class GerenciarCaixa {
     public static void main(String[] args) {
         JFrame tela = new JFrame();
+        /*
         tela.setContentPane(new TelaMovimento2().getPainelCaixa());
         tela.setVisible(true);
         tela.setTitle("Controle de Caixa");
         tela.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         tela.setLocationRelativeTo(null);
         tela.setSize(350,400);
-        //TelaMovimento tela = new TelaMovimento();
-        //tela.setVisible(true);
+        */
+        TelaMovimento tela = new TelaMovimento();
+        tela.setVisible(true);
     }
 }
